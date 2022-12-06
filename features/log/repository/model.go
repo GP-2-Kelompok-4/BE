f10bce6daa86b2964c47e4ba8fc957ae1cc81cc4
aziz-wahyudin <midnightmist37@gmail.com>
2022-12-06 15:25:54 +0700

    add log model

---
+++ features/log/repository/model.go
@@ -0,0 +1,52 @@
+package repository
+
+import (
+	"time"
+
+	"gorm.io/gorm"
+)
+
+type Log struct {
+	gorm.Model
+	UserID   uint
+	MenteeID uint
+	Notes    string
+	Status   string
+}
+
+type User struct {
+	gorm.Model
+	Name         string
+	Email        string
+	Password     string
+	Phone_Number string
+	Role         string
+	Address      string
+	Home_Address string
+	Team         string
+	Status       string
+	Gender       string
+	Log          []Log
+}
+
+type Mentee struct {
+	gorm.Model
+	Name                   string
+	Nickname               string
+	Email                  string
+	Gender                 string
+	PhoneNumber            string
+	Address                string
+	HomeAddress            string
+	TelegramAccount        string
+	DiscordAccount         string
+	Status                 string
+	EducationType          string
+	EducationMajor         string
+	Graduate               time.Time
+	Institution            string
+	EmergencyContact       string
+	EmergencyContactStatus string
+	ClassID                uint
+	Log                    []Log
+}
