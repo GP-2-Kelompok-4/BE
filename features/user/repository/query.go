09c4f307b5cafdea5152e866f3f7005f893c7749
Achmad Qizwini <akise.shun@gmail.com>
2022-12-06 16:21:56 +0700

    update query user

--- features/user/repository/query.go
+++ features/user/repository/query.go
@@ -18,6 +18,17 @@ func New(db *gorm.DB) user.RepositoryInterface {
 	}
 }
 
+func (repo *userRepository) GetAllUser() (data []user.Core, err error) {
+	var users []User
+
+	tx := repo.db.Find(&users)
+	if tx.Error != nil {
+		return nil, tx.Error
+	}
+	var dataCore = toCoreList(users)
+	return dataCore, nil
+}
+
 func (repo *userRepository) AddUser(input user.Core) (row int, err error) {
 	userGorm := fromCore(input)
 	tx := repo.db.Create(&userGorm) // proses insert data
@@ -28,4 +39,29 @@ func (repo *userRepository) AddUser(input user.Core) (row int, err error) {
 		return 0, errors.New("insert failed")
 	}
 	return int(tx.RowsAffected), nil
+}
+
+func (repo *userRepository) UpdateUser(datacore user.Core, id int) (err error) {
+	userGorm := fromCore(datacore)
+	tx := repo.db.Where("id= ?", id).Updates(userGorm)
+	if tx.Error != nil {
+		return tx.Error
+	}
+	if tx.RowsAffected == 0 {
+		return errors.New("update user failed")
+	}
+	return nil
+}
+
+func (repo *userRepository) DeleteUser(id int) (err error) {
+	idUser := User{}
+
+	tx := repo.db.Delete(&idUser, id)
+	if tx.Error != nil {
+		return tx.Error
+	}
+	if tx.RowsAffected == 0 {
+		return errors.New("delete user by id failed")
+	}
+	return nil
 }
