4f7782a792634908b036c386e05c9ad9e713f637
Achmad Qizwini <akise.shun@gmail.com>
2022-12-06 11:54:22 +0700

    update user model

--- features/user/repository/model.go
+++ features/user/repository/model.go
@@ -1,1 +1,36 @@
 package repository
+
+import (
+	_user "github.com/GP-2-Kelompok-4/Immersive-Dashboard-App/features/user"
+	"gorm.io/gorm"
+)
+
+type User struct {
+	gorm.Model
+	Name         string
+	Email        string
+	Password     string
+	Phone_Number string
+	Role         string
+	Address      string
+	Home_Address string
+	Team         string
+	Status       string
+	Gender       string
+}
+
+func fromCore(dataCore _user.Core) User {
+	userGorm := User{
+		Name:         dataCore.Name,
+		Email:        dataCore.Email,
+		Password:     dataCore.Password,
+		Phone_Number: dataCore.Phone_Number,
+		Role:         dataCore.Role,
+		Address:      dataCore.Address,
+		Home_Address: dataCore.Home_Address,
+		Team:         dataCore.Team,
+		Status:       dataCore.Status,
+		Gender:       dataCore.Gender,
+	}
+	return userGorm
+}
