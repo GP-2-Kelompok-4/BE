55ef740a4de9a5f98c8a8058abcd8051f255e393
Achmad Qizwini <akise.shun@gmail.com>
2022-12-06 16:21:22 +0700

    update model user

--- features/user/repository/model.go
+++ features/user/repository/model.go
@@ -34,3 +34,30 @@ func fromCore(dataCore _user.Core) User {
 	}
 	return userGorm
 }
+
+func (dataModel *User) toCore() _user.Core {
+	return _user.Core{
+		ID:           dataModel.ID,
+		Name:         dataModel.Name,
+		Email:        dataModel.Email,
+		Password:     dataModel.Password,
+		Phone_Number: dataModel.Phone_Number,
+		Role:         dataModel.Role,
+		Address:      dataModel.Address,
+		Home_Address: dataModel.Home_Address,
+		Team:         dataModel.Team,
+		Status:       dataModel.Status,
+		Gender:       dataModel.Gender,
+		CreatedAt:    dataModel.CreatedAt,
+		UpdatedAt:    dataModel.UpdatedAt,
+	}
+}
+
+// mengubah slice struct model gorm ke slice struct core
+func toCoreList(dataModel []User) []_user.Core {
+	var dataCore []_user.Core
+	for _, v := range dataModel {
+		dataCore = append(dataCore, v.toCore())
+	}
+	return dataCore
+}
