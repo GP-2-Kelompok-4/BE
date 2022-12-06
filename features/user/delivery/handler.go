47e61e26ca9000a849c5ea8ba4782fead95279c4
Achmad Qizwini <akise.shun@gmail.com>
2022-12-06 11:51:32 +0700

    add handler user

--- features/user/delivery/handler.go
+++ features/user/delivery/handler.go
@@ -1,1 +1,35 @@
 package delivery
+
+import (
+	"net/http"
+
+	"github.com/GP-2-Kelompok-4/Immersive-Dashboard-App/features/user"
+	"github.com/GP-2-Kelompok-4/Immersive-Dashboard-App/utils/helper"
+	"github.com/labstack/echo/v4"
+)
+
+type UserDelivery struct {
+	userService user.ServiceInterface
+}
+
+func New(service user.ServiceInterface, e *echo.Echo) {
+	handler := &UserDelivery{
+		userService: service,
+	}
+
+	e.POST("/users", handler.AddUser)
+}
+
+func (delivery *UserDelivery) AddUser(c echo.Context) error {
+	userInput := UserRequest{}
+	errBind := c.Bind(&userInput)
+	if errBind != nil {
+		return c.JSON(http.StatusBadRequest, helper.FailedResponse("Error binding data "+errBind.Error()))
+	}
+	dataCore := requestToCore(userInput)
+	err := delivery.userService.AddUser(dataCore)
+	if err != nil {
+		return c.JSON(http.StatusInternalServerError, helper.FailedResponse("failed insert data"+err.Error()))
+	}
+	return c.JSON(http.StatusOK, helper.SuccessResponse("success create new users"))
+}
