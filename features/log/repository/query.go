db674cc828c827d8baf43449f555e54a2ac63d2f
Achmad Qizwini <akise.shun@gmail.com>
2022-12-07 23:10:39 +0700

    update log feature

--- features/log/repository/query.go
+++ features/log/repository/query.go
@@ -1,1 +1,31 @@
 package repository
+
+import (
+	"errors"
+
+	"github.com/GP-2-Kelompok-4/Immersive-Dashboard-App/features/log"
+	"gorm.io/gorm"
+)
+
+type logRepository struct {
+	db *gorm.DB
+}
+
+func New(db *gorm.DB) log.RepositoryInterface {
+	return &logRepository{
+		db: db,
+	}
+}
+
+// CreateLog implements log.RepositoryInterface
+func (repo *logRepository) CreateLog(data log.CoreLog) (err error) {
+	classGorm := fromCore(data)
+	tx := repo.db.Create(&classGorm)
+	if tx.Error != nil {
+		return tx.Error
+	}
+	if tx.RowsAffected == 0 {
+		return errors.New("insert failed")
+	}
+	return nil
+}
--- features/log/service/logic.go
+++ features/log/service/logic.go
@@ -1,6 +1,10 @@
 package service
 
-import "github.com/GP-2-Kelompok-4/Immersive-Dashboard-App/features/log"
+import (
+	"errors"
+
+	"github.com/GP-2-Kelompok-4/Immersive-Dashboard-App/features/log"
+)
 
 type logService struct {
 	logRepository log.RepositoryInterface
@@ -14,5 +18,10 @@ func New(repo log.RepositoryInterface) log.ServiceInterface {
 
 // CreateLogs implements log.ServiceInterface
 func (repo *logService) CreateLog(data log.CoreLog) (err error) {
-	panic("unimplemented")
+	errCreate := repo.logRepository.CreateLog(data)
+	if errCreate != nil {
+		return errors.New("failed create log, error query")
+	}
+	return nil
+
 }
