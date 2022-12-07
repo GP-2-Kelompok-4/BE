b773f94adb1c64783ce21aa50a7343d24bf2ec71
Achmad Qizwini <akise.shun@gmail.com>
2022-12-07 13:43:35 +0700

    update login query

--- features/auth/repository/query.go
+++ features/auth/repository/query.go
@@ -18,34 +18,38 @@ func New(db *gorm.DB) auth.RepositoryInterface {
 }
 
 func (repo *authRepo) Login(email, password string) (loginData auth.Core, err error) {
-
-	var userData = User{}
-	tx := repo.db.Where("email = ? AND password = ?", email, password).First(&userData)
+	userModel := User{}
+	loginData = auth.Core{}
+	tx := repo.db.Where("email = ? AND password = ?", email, password).First(&userModel)
 	if tx.Error != nil {
 		return loginData, tx.Error
 	}
 	if tx.RowsAffected == 0 {
 		return loginData, errors.New("login failed")
 	}
-	loginData = ToCore(userData)
 
+	loginData = ToCore(userModel)
 	return loginData, nil
+
+	// var userData Users
+	// tx := repo.db.Where("email = ?", email).First(&userData)
+	// if tx.Error != nil {
+	// 	return "", tx.Error
+	// }
+
+	// if tx.RowsAffected == 0 {
+	// 	return "", errors.New("login failed")
+	// }
+
+	// cekpass := helper.CheckPasswordHash(password, userData.Password)
+	// if !cekpass {
+	// 	return "", errors.New("login failed")
+	// }
+
+	// token, errToken := middlewares.CreateToken(int(userData.ID), userData.Role)
+	// if errToken != nil {
+	// 	return "", errToken
+	// }
+
+	// return token, nil
 }
-
-// func Login(email, password string) (string, error) {
-// 	var userData models.User
-// 	tx := config.DB.Where("email = ? AND password = ?", email, password).First(&userData)
-// 	if tx.Error != nil {
-// 		return "", tx.Error
-// 	}
-// 	if tx.RowsAffected == 0 {
-// 		return "", errors.New("login failed")
-// 	}
-
-// 	token, errToken := middlewares.CreateToken(int(userData.ID), userData.Role)
-// 	if errToken != nil {
-// 		return "", errToken
-// 	}
-
-// 	return token, nil
-// }
