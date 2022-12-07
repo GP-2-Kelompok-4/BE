b684c4529c3469eeedd06ad20b14ba80ce57b2c7
Achmad Qizwini <akise.shun@gmail.com>
2022-12-07 09:49:43 +0700

    update auth feature

--- features/auth/repository/query.go
+++ features/auth/repository/query.go
@@ -4,35 +4,48 @@ import (
 	"errors"
 
 	"github.com/GP-2-Kelompok-4/Immersive-Dashboard-App/features/auth"
-	"github.com/GP-2-Kelompok-4/Immersive-Dashboard-App/middlewares"
 	"gorm.io/gorm"
 )
 
-type authData struct {
+type authRepo struct {
 	db *gorm.DB
 }
 
 func New(db *gorm.DB) auth.RepositoryInterface {
-	return &authData{
+	return &authRepo{
 		db: db,
 	}
 }
 
-func (repo *authData) Login(email, password string) (loginData auth.Core, err error) {
+func (repo *authRepo) Login(email, password string) (loginData auth.Core, err error) {
 
-	var userData User
+	var userData = User{}
 	tx := repo.db.Where("email = ? AND password = ?", email, password).First(&userData)
 	if tx.Error != nil {
-		return "", tx.Error
+		return loginData, tx.Error
 	}
 	if tx.RowsAffected == 0 {
-		return "", errors.New("login failed")
+		return loginData, errors.New("login failed")
 	}
+	loginData = ToCore(userData)
 
-	token, errToken := middlewares.CreateToken(int(userData.ID), userData.Role)
-	if errToken != nil {
-		return "", errToken
-	}
-
-	return token, nil
+	return loginData, nil
 }
+
+// func Login(email, password string) (string, error) {
+// 	var userData models.User
+// 	tx := config.DB.Where("email = ? AND password = ?", email, password).First(&userData)
+// 	if tx.Error != nil {
+// 		return "", tx.Error
+// 	}
+// 	if tx.RowsAffected == 0 {
+// 		return "", errors.New("login failed")
+// 	}
+
+// 	token, errToken := middlewares.CreateToken(int(userData.ID), userData.Role)
+// 	if errToken != nil {
+// 		return "", errToken
+// 	}
+
+// 	return token, nil
+// }
