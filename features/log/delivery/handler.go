5c47c2331bb9478fa2083b4ef816ea1b9ce16fce
aziz-wahyudin <midnightmist37@gmail.com>
2022-12-09 00:19:05 +0700

    update handler log

--- features/log/delivery/handler.go
+++ features/log/delivery/handler.go
@@ -22,23 +22,36 @@ func New(service log.ServiceInterface, e *echo.Echo) {
 
 func (delivery *LogDelivery) CreateLog(c echo.Context) error {
 	userId := middlewares.ExtractTokenUserId(c)
-	Poster := middlewares.ExtractTokenUserName(c)
-	
+	name := middlewares.ExtractTokenUserName(c)
+	helper.LogDebug("\n extracttokenname= ", name)
+
 	if userId == 0 {
 		return c.JSON(http.StatusNotFound, helper.FailedResponse("requested resource was not found"))
 	}
 
 	logInput := LogRequest{}
+	logInput.UserName = name
+	logInput.UserID = uint(userId)
+
+	helper.LogDebug("\n extracttokenname= ", logInput.UserName)
+
 	errBind := c.Bind(&logInput)
 	if errBind != nil {
 		return c.JSON(http.StatusNotFound, helper.FailedResponse("requested resource was not found "+errBind.Error()))
 	}
-	
-	
+
+	logInput.UserName = name
+	logInput.UserID = uint(userId)
+
+	helper.LogDebug("\n extracttokenname= ", logInput.UserName)
+
 	logCore := requestToCore(logInput)
+
 	logCore.UserID = uint(userId)
-	logCore.UserName = Poster
-	
+
+	logCore.UserName = name
+	helper.LogDebug("\n extracttokenname= ", logCore.UserName)
+
 	err := delivery.logService.CreateLog(logCore)
 	if err != nil {
 		return c.JSON(http.StatusInternalServerError, helper.FailedResponse("internal server error "+err.Error()))
